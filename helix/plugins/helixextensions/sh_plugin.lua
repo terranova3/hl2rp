f739bc7786534a62f6928c3832a08ac74ee5ee63
Zach <48750526+zacharyenriquee@users.noreply.github.com>
2020-04-30 00:34:08 +1000

    Adds color functionality to classes, allowing the scoreboard to show class > faction for coloring and organisation

---
+++ helix/plugins/helixextensions/sh_plugin.lua
@@ -0,0 +1,9 @@
+--[[
+	© 2020 TERRANOVA do not share, re-distribute or modify
+	without permission of its author (zacharyenriquee@gmail.com).
+--]]
+
+PLUGIN.name = "Helix Extensions";
+PLUGIN.description = "Various extensions to implement various features not native to the default framework. Includes custom classes and class based scoreboard.";
+PLUGIN.author = "Adolphus";
+PLUGIN.maxLength = 512;
