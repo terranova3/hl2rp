49ebe85fa5face10c7ff451cda9cf2fb01170b28
Zach <48750526+zacharyenriquee@users.noreply.github.com>
2020-05-03 05:04:06 +1000

    Civil Protection coats and gasmasks

---
+++ ixhl2rp/plugins/cpsystem/plugin/items/sh_cp_gasmask_3.lua
@@ -0,0 +1,14 @@
+--[[
+	© 2020 TERRANOVA do not share, re-distribute or modify
+	without permission of the author.
+--]]
+
+local PLUGIN = PLUGIN;
+
+ITEM.base = "base_cp_gasmask";
+ITEM.name = "[PH] Gasmask #3";
+ITEM.model = "models/newcca/gasmask3.mdl";
+ITEM.description = "[PH] Needs Description";
+ITEM.bodyGroups = {
+	["gasmask"] = 3,
+}
