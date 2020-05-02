1211529a1be4bbf9254bc899413117927a2289b7
Muzkito <51123726+Muzkito@users.noreply.github.com>
2020-05-03 09:01:50 +1000

    Update sh_plugin.lua

--- ixhl2rp/plugins/passivedispatch/sh_plugin.lua
+++ ixhl2rp/plugins/passivedispatch/sh_plugin.lua
@@ -3,4 +3,4 @@ local PLUGIN = PLUGIN;
 PLUGIN.name = "Passive Dispatch"
 PLUGIN.author = "muz"
 PLUGIN.description = "Occasionally emits a Dispatch Announcement."
-ix.util.Include("sv_hooks.lua")
+ix.util.Include("sv_hooks.lua");
