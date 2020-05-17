6edf1e6084a2d001aa8db9e2b7c82eca334346cc
Cooldeath91 <surfenbird@hotmail.com>
2020-05-17 22:56:01 +0800

    Testing IX Crafting Items
    Making sure the itemspawner blah blah and items work

---
+++ ixhl2rp/plugins/ixcraft/items/sh_combine_battery.lua
@@ -0,0 +1,9 @@
+
+ITEM.name = "Combine Battery"
+ITEM.model = Model("models/Items/battery.mdl")
+ITEM.description = "A combine battery, it's potential is unknown."
+ITEM.width = 1
+ITEM.height = 2
+ITEM.price = 150
+ITEM.category = "Crafting"
+ITEM.rarity = "Rare"
