5381bd1df9172dd72e15d8129ba288771a6c084a
Cooldeath91 <surfenbird@hotmail.com>
2020-05-19 22:27:33 +0800

    Ixcraft Finish Crafting items
    Finished all crafting items added them to master. Recipies soon to
    follow

---
+++ ixhl2rp/plugins/ixcraft/items/sh_gun_magazine.lua
@@ -0,0 +1,8 @@
+
+ITEM.name = "Magazine"
+ITEM.model = Model("models/weapons/tfa_mmod/w_smg1_magazine.mdl")
+ITEM.description = "This part is where the ammo is stored for weapons."
+ITEM.width = 1
+ITEM.height = 1
+ITEM.price = 75
+ITEM.category = "Crafting"
---
+++ ixhl2rp/plugins/ixcraft/items/sh_locker_door.lua
@@ -0,0 +1,9 @@
+
+ITEM.name = "Locker Door"
+ITEM.model = Model("models/props_lab/lockerdoorleft.mdl")
+ITEM.description = "Pure Junk, better sell this for some tokens."
+ITEM.width = 1
+ITEM.height = 2
+ITEM.price = 100
+ITEM.category = "Crafting"
+ITEM.rarity = "Rare"
