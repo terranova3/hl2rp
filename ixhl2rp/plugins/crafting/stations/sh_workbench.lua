442b735f14b567f0f69d6c6a2b7e3defc8093dee
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-07-06 22:49:00 +1000

    ADDITION: New crafting with entities, base work.

---
+++ ixhl2rp/plugins/crafting/stations/sh_workbench.lua
@@ -0,0 +1,3 @@
+STATION.name = "Workbench"
+STATION.description = "A workbench used for crafting."
+STATION.model = "models/props_wasteland/controlroom_desk001b.mdl"
--- ixhl2rp/plugins/medical/items/sh_splint.lua
+++ ixhl2rp/plugins/medical/items/sh_splint.lua
@@ -61,7 +61,7 @@ end
 function ITEM:PopulateTooltip(tooltip)
 	local data = tooltip:AddRow("data")
 	data:SetBackgroundColor(Color(190, 62, 39, 120))
-	data:SetText("Uses Left: " .. self:GetData("currentUses"))
+	data:SetText("Uses Left: " .. self:GetData("currentUses", self.uses))
 	data:SetFont("BudgetLabel")
 	data:SetExpensiveShadow(0.5)
 	data:SizeToContents()
