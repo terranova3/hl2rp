ab4f2d7dc05dc2ddcdf015e8c5ee6b5364589ffe
fnn22 <64693109+fnn22@users.noreply.github.com>
2020-07-06 21:47:23 +0800

    ADDITION: new containers
    Added a bunch of new containers, inc. ones for mass storage and specific roleplay (coffee pot).

---
+++ ixhl2rp/plugins/consumables/items/container_liquid/sh_pet_bottle.lua
@@ -0,0 +1,11 @@
+--[[
+	© 2020 TERRANOVA do not share, re-distribute or modify
+	without permission of its author.
+--]]
+
+ITEM.name = "PET Bottle";
+ITEM.model = "models/warz/consumables/water_l.mdl";
+ITEM.width = 1;
+ITEM.height = 2;
+ITEM.description = "A large plastic bottle designed for holding liquid.";
+ITEM.capacity = 1000
