16068b8a276a5cf82f5e51c160438a8337151e4b
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-07-07 23:00:48 +1000

    UPDATE: Fixed linter issues from previous commits

--- ixhl2rp/plugins/clothing/items/legs/sh_rebel_jeans_1.lua
+++ ixhl2rp/plugins/clothing/items/legs/sh_rebel_jeans_1.lua
@@ -1,4 +1,4 @@
-d--[[
+--[[
 	© 2020 TERRANOVA do not share, re-distribute or modify
 	without permission of its author.
 --]]
@@ -6,7 +6,7 @@ d--[[
 ITEM.base = "base_legs";
 ITEM.name = "Rebel Legs - 1";
 ITEM.model = "models/fty/items/greenrebelpants.mdl"
-ITEM.description = "Worn and re-purposed levis made to aid the active rebellion against the opposition, fitted with multi-purpose pouches able to house ammunition and small arms while tucked into the ankles of some combat boots. Stained with grime and faded colors. "
+ITEM.description = "Worn and re-purposed levis made to aid the active rebellion against the opposition, fitted with multi-purpose pouches able to house ammunition and small arms while tucked into the ankles of some combat boots. Stained with grime and faded colors."
 ITEM.bodyGroups = {
 	["legs"] = 5
 }
