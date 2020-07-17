9a17668be746de6c5042e937654d6df6312b49ae
websthewallflower <64693232+websthewallflower@users.noreply.github.com>
2020-07-17 11:16:11 +1000

    UPDATE: Small update
    Removed badge item | Added bag bodygroup - for later use

---
+++ ixhl2rp/plugins/clothing/items/bag/sh_blackdufflebag.lua
@@ -0,0 +1,11 @@
+--[[
+	© 2020 TERRANOVA do not share, re-distribute or modify
+	without permission of its author.
+--]]
+
+ITEM.base = "base_bag";
+ITEM.name = "Black Duffle Bag";
+ITEM.description = "A black duffle bag with a long strap that allows the carrier to let the bag rest at their side."
+ITEM.bodyGroups = {
+	["bag"] = 1
+}
