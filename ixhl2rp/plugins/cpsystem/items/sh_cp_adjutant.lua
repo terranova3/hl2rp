50d5adbc512fc759009e913af0ac760afe73301e
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-07-17 14:24:11 +1000

    ADDITION: Adjutant 'uniform'

---
+++ ixhl2rp/plugins/cpsystem/items/sh_cp_adjutant.lua
@@ -0,0 +1,27 @@
+--[[
+	© 2020 TERRANOVA do not share, re-distribute or modify
+	without permission of its author (zacharyenriquee@gmail.com).
+--]]
+
+local PLUGIN = PLUGIN;
+
+ITEM.name = "Civil Protection Adjutant Uniform";
+ITEM.base = "base_cp_uniform"
+ITEM.category = "Clothing";
+ITEM.description = "A complete uniform kit that includes a vulcanized rubber flak jacket, polyester metropolice slacks, kevlar vest, steel toed jackboots, rubber gloves and more.";
+ITEM.maxArmor = 100;
+
+function ITEM:OnGetReplacement()
+	local client = self:GetOwner()
+	local model = client:GetModel()
+	local newModel = ""
+
+	if(string.find(model, "models/ug/new/citizens/")) then
+		newModel = string.Replace(model, "models/ug/new/citizens/", "models/cmbofficers/female/")
+		newModel = string.Replace(newModel, ".mdl", "_cmbofficer.mdl")
+
+		return newModel
+	end
+
+	return model
+end 
