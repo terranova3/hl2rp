a132df58da45eab6579ce751abc089121eb8e132
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-07-17 14:34:39 +1000

    ADDITION: Auto determine if use CP female model

--- ixhl2rp/plugins/cpsystem/items/sh_cp_standard.lua
+++ ixhl2rp/plugins/cpsystem/items/sh_cp_standard.lua
@@ -7,7 +7,18 @@ local PLUGIN = PLUGIN;
 
 ITEM.name = "Civil Protection Uniform";
 ITEM.base = "base_cp_uniform"
-ITEM.replacement = "models/ma/hla/terranovapolice.mdl";
 ITEM.category = "Clothing";
 ITEM.description = "A complete uniform kit that includes a vulcanized rubber flak jacket, polyester metropolice slacks, kevlar vest, steel toed jackboots, rubber gloves and more.";
 ITEM.maxArmor = 100;
+
+function ITEM:OnGetReplacement()
+	local client = self:GetOwner()
+	local model = client:GetModel()
+	local newModel = "models/ma/hla/terranovapolice.mdl"
+
+	if(string.find(model, "female")) then
+		return "models/ma/hla/terranovafemalepolice.mdl"
+	end
+
+	return newModel
+end 
