cc7839f41f2923ad689d7d8d3fd17c09a3808aaa
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-07-17 15:03:10 +1000

    UPDATE: Overwatch default model changed

--- ixhl2rp/plugins/overwatch/faction/sh_ota.lua
+++ ixhl2rp/plugins/overwatch/faction/sh_ota.lua
@@ -3,7 +3,7 @@ FACTION.name = "Overwatch Transhuman Arm"
 FACTION.description = "A transhuman Overwatch soldier produced by the Combine."
 FACTION.color = Color(150, 50, 50, 255)
 FACTION.pay = 40
-FACTION.models = {"models/combine_soldier.mdl"}
+FACTION.models = {"models/cultist/hl_a/vannila_combine/npc/combine_soldier.mdl"}
 FACTION.isDefault = false
 FACTION.isGloballyRecognized = true
 FACTION.runSounds = {[0] = "NPC_CombineS.RunFootstepLeft", [1] = "NPC_CombineS.RunFootstepRight"}
