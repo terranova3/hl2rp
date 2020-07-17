d6c162c177243e5e1c973ad5f216f975e3902762
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-07-17 15:02:51 +1000

    ADDITION: Overwatch model animations

--- ixhl2rp/plugins/overwatch/sh_plugin.lua
+++ ixhl2rp/plugins/overwatch/sh_plugin.lua
@@ -6,3 +6,18 @@
 PLUGIN.name = "Overwatch";
 PLUGIN.description = "Replaces the old overwatch and adds ranks and automatic naming.";
 PLUGIN.author = "Adolphus";
+
+ix.anim.SetModelClass("models/cultist/hl_a/vannila_combine/npc/combine_soldier.mdl", "overwatch");
+ix.anim.SetModelClass("models/hlvr/characters/worker/npc/worker_combine.mdl", "overwatch");
+ix.anim.SetModelClass("models/hlvr/characters/hazmat_worker/npc/hazmat_worker_combine.mdl", "overwatch");
+ix.anim.SetModelClass("models/characters/combine_soldier/jqblk/combine_s_super.mdl", "overwatch");
+ix.anim.SetModelClass("models/cultist/hl_a/combine_pilot.mdl", "overwatch");
+ix.anim.SetModelClass("models/cultist/hl_a/combine_commander/npc/combine_commander.mdl", "overwatch");
+ix.anim.SetModelClass("models/cultist/hl_a/combine_grunt/combine_grunthazmatlead.mdl", "overwatch");
+ix.anim.SetModelClass("models/cultist/hl_a/combine_grunt/combine_grunthazmatregular.mdl", "overwatch");
+ix.anim.SetModelClass("models/cultist/hl_a/combine_heavy/combine_heavy_trooper.mdl", "overwatch");
+ix.anim.SetModelClass("models/cultist/hl_a/combine_suppresor/combine_suppresorterranova.mdl", "overwatch");
+ix.anim.SetModelClass("models/combine_sniper.mdl", "overwatch");
+ix.anim.SetModelClass("models/combine_sniper_2.mdl", "overwatch");
+
+
