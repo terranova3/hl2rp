dae487d4682fbd46f3801d34b2676ef5ec2ef483
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-07-20 01:44:06 +1000

    ADDITION: Openable example - ration

---
+++ ixhl2rp/plugins/consumables/items/openable/sh_ration.lua
@@ -0,0 +1,15 @@
+ITEM.name = "Ration"
+ITEM.model = Model("models/weapons/w_package.mdl")
+ITEM.description = "A shrink-wrapped packet containing some food and money."
+ITEM.contains = {
+    [1] = {
+        uniqueID = "supplements",
+        amount = 1,
+        data = {}
+    },
+    [2] = {
+        uniqueID = "water",
+        amount = 1,
+        data = {}
+    }
+}
