51bc1c4edf1476eacc2574fdc55b602108457683
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-07-22 04:02:10 +1000

    ADDITION: Added gloves models to their items

--- ixhl2rp/plugins/clothing/items/hands/sh_fingerless_gloves.lua
+++ ixhl2rp/plugins/clothing/items/hands/sh_fingerless_gloves.lua
@@ -5,6 +5,7 @@
 
 ITEM.base = "base_hands";
 ITEM.name = "Fingerless Gloves";
+ITEM.model = "models/fty/items/gloves.mdl"
 ITEM.description = "A set of fingerless gloves made from a synthetic-woolen blend, providing comfortable insulation. The symbol of the CWU is imprinted on the back of the glove in a slightly darker colour."
 ITEM.bodyGroups = {
 	["hands"] = 1
