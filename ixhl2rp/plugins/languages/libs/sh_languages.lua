3885249384e5bef9da83f8c8517e3f64f44fe70c
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-07-21 21:36:36 +1000

    ADDITION: Language automatic item generation

--- ixhl2rp/plugins/languages/libs/sh_languages.lua
+++ ixhl2rp/plugins/languages/libs/sh_languages.lua
@@ -23,7 +23,7 @@ end
 function ix.language.Register()
     for abbreviation, language in pairs(ix.language.stored) do
         ix.chat.Register(abbreviation, {
-			format = "%s says in " .. language .. ": \"%s\"",
+			format = "%s speaks in " .. language .. ": \"%s\"",
 			indicator = "chatTalking",
 			GetColor = function(self, speaker, text)
 				-- If you are looking at the speaker, make it greener to easier identify who is talking.
@@ -39,7 +39,7 @@ function ix.language.Register()
             description = "Speaking in ".. language,
             CanSay = function(self, speaker, text)
                 if(!speaker:GetCharacter():HasLanguage(language)) then
-                    speaker:NotifyLocalized(string.format("You don't have the %s language.", language))
+                    speaker:NotifyLocalized(string.format("You don't know how to speak %s.", language))
                     
                     return false
                 end
@@ -53,7 +53,44 @@ function ix.language.Register()
                     chat.AddText(self.color, string.format("%s says something unintelligible in %s.", speaker:GetName(), text))
                 end
             end
-		})
-
+        })
+        
+        ix.language.RegisterItem(abbreviation, language)
     end
 end
+
+function ix.language.RegisterItem(k, v)
+	local ITEM = ix.item.Register( "book_" .. k, nil, nil, nil, true)
+    ITEM.name = v .. " For Dummies"
+    ITEM.description = string.format("A hefty book packed with information about the %s language.", v)
+    ITEM.model = "models/gibs/props_office/books_3_gib1.mdl"
+    ITEM.category = "Literature"
+    ITEM.language = v
+    ITEM.functions.learn = {
+        name = "Learn Language",
+        icon = "icon16/book_open.png",
+        OnRun = function(itemTable, data)
+            local character = itemTable.player:GetCharacter()
+
+            if(character:HasLanguage(itemTable.language)) then
+                itemTable.player:Notify(string.format("You already know the %s language!", itemTable.language))
+
+                return false
+            end
+
+            character:AddLanguage(itemTable.language)
+            itemTable.player:Notify(string.format("You have learned the %s language.", itemTable.language))
+        end
+    }
+
+    function ITEM:GetModel()
+        local models = {
+            "models/gibs/props_office/books_3_gib2.mdl",
+            "models/gibs/props_office/books_3_gib3.mdl",
+            self.model
+        }
+
+        return models[math.random(1, 3)]
+    end
+end
+
