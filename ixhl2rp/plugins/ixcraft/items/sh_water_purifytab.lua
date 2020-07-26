ddeb21d4f8bfbf25cb5145139d315a776bee9d71
Cooldeath91 <surfenbird@hotmail.com>
2020-07-26 15:55:08 +0800

    Update: Added CWU Work cycle Recipies
    More recipies for workcycles, more comming 1/2

---
+++ ixhl2rp/plugins/ixcraft/items/sh_water_purifytab.lua
@@ -0,0 +1,8 @@
+
+ITEM.name = "Civil-Approved Purfication Tablets"
+ITEM.model = Model("models/props_lab/jar01b.mdl")
+ITEM.description = "A plastic jar with large tablets, it has a biohazard symbol labled onto the jar."
+ITEM.width = 1
+ITEM.height = 1
+ITEM.price = 5
+ITEM.category = "Crafting"
