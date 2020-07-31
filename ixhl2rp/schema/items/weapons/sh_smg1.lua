d6987749675bb253378b6c846bb75c4d4122c5ad
Pointmanman <68431598+Pointmanman@users.noreply.github.com>
2020-07-31 16:58:20 +1000

    Update sh_smg1.lua
    UPDATE: New MP7 model, pacData, iconCam

--- ixhl2rp/schema/items/weapons/sh_smg1.lua
+++ ixhl2rp/schema/items/weapons/sh_smg1.lua
@@ -1,12 +1,83 @@
 ITEM.name = "MP7"
 ITEM.description = "A compact, fully automatic sub-machine gun."
-ITEM.model = "models/weapons/w_smg1.mdl"
-ITEM.class = "weapon_smg1"
+ITEM.model = "models/weapons/tnmmod/w_smg1.mdl"
+ITEM.class = "arccw_mp7terra"
 ITEM.weaponCategory = "primary"
 ITEM.classes = {CLASS_EMP, CLASS_EOW}
 ITEM.flag = "V"
-ITEM.width = 3
+ITEM.width = 4
 ITEM.height = 2
+ITEM.pacData = {
+	[1] = {
+		["children"] = {
+			[1] = {
+				["children"] = {
+				},
+				["self"] = {
+					["Skin"] = 0,
+					["Invert"] = false,
+					["LightBlend"] = 1,
+					["CellShade"] = 0,
+					["OwnerName"] = "self",
+					["AimPartName"] = "",
+					["IgnoreZ"] = false,
+					["AimPartUID"] = "",
+					["Passes"] = 1,
+					["Name"] = "",
+					["NoTextureFiltering"] = false,
+					["DoubleFace"] = false,
+					["PositionOffset"] = Vector(0, 0, 0),
+					["IsDisturbing"] = false,
+					["Fullbright"] = false,
+					["EyeAngles"] = false,
+					["DrawOrder"] = 0,
+					["TintColor"] = Vector(0, 0, 0),
+					["UniqueID"] = "1644819129",
+					["Translucent"] = false,
+					["LodOverride"] = -1,
+					["BlurSpacing"] = 0,
+					["Alpha"] = 1,
+					["Material"] = "",
+					["UseWeaponColor"] = false,
+					["UsePlayerColor"] = false,
+					["UseLegacyScale"] = false,
+					["Bone"] = "spine",
+					["Color"] = Vector(255, 255, 255),
+					["Brightness"] = 1,
+					["BoneMerge"] = false,
+					["BlurLength"] = 0,
+					["Position"] = Vector(5.61328125, -4.640380859375, 0.069747924804688),
+					["AngleOffset"] = Angle(0, 0, 0),
+					["AlternativeScaling"] = false,
+					["Hide"] = false,
+					["OwnerEntity"] = false,
+					["Scale"] = Vector(1, 1, 1),
+					["ClassName"] = "model",
+					["EditorExpand"] = false,
+					["Size"] = 0.85,
+					["ModelFallback"] = "",
+					["Angles"] = Angle(-18.622611999512, -174.59838867188, -6.4337058067322),
+					["TextureFilter"] = 3,
+					["Model"] = "models/weapons/tnmmod/w_smg1.mdl",
+					["BlendMode"] = "",
+				},
+			},
+		},
+		["self"] = {
+			["DrawOrder"] = 0,
+			["UniqueID"] = "186800644",
+			["AimPartUID"] = "",
+			["Hide"] = false,
+			["Duplicate"] = false,
+			["ClassName"] = "group",
+			["OwnerName"] = "self",
+			["IsDisturbing"] = false,
+			["Name"] = "my outfit",
+			["EditorExpand"] = true,
+		},
+	},
+}
+
 ITEM.iconCam = {
 	ang	= Angle(-0.020070368424058, 270.40155029297, 0),
 	fov	= 7.2253324508038,
