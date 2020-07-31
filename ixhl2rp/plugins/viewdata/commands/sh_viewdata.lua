72e23a753dacaa0d56f426cb33885dc114d8f3fc
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-07-31 21:03:24 +1000

    UPDATE: Removed viewdata dev testing hook

--- ixhl2rp/plugins/viewdata/commands/sh_viewdata.lua
+++ ixhl2rp/plugins/viewdata/commands/sh_viewdata.lua
@@ -9,9 +9,9 @@ ix.command.Add("ViewData", {
 		ix.type.character
 	},
 	OnRun = function(self, client, target)
-		--if (!hook.Run("CanPlayerViewData", client, targetClient)) then
-		--	return "@cantViewData"
-		--end
+		if (!hook.Run("CanPlayerViewData", client, targetClient)) then
+			return "@cantViewData"
+		end
 		
 		net.Start("ixViewdataInitiate")
 			net.WriteInt(target.id, 16) -- We can actually save on data by just sending the character id. The client can index it from char loaded table.
