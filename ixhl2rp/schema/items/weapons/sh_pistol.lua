1006bf72c2df795a06c1051af99c3f6499cebe7b
Pointmanman <68431598+Pointmanman@users.noreply.github.com>
2020-07-31 16:57:31 +1000

    Update sh_pistol.lua
    UPDATE: New pistol model, pacData, needs iconCam

--- ixhl2rp/schema/items/weapons/sh_pistol.lua
+++ ixhl2rp/schema/items/weapons/sh_pistol.lua
@@ -1,18 +1,12 @@
-ITEM.name = ".9mm Pistol"
-ITEM.description = "A sidearm utilising 9mm Ammunition."
-ITEM.model = "models/weapons/w_pistol.mdl"
-ITEM.class = "weapon_pistol"
+ITEM.name = "USP Match"
+ITEM.description = "A German pistol appropriated by the Combine. It now sees usage under the Overwatch as their standard issue sidearm."
+ITEM.model = "models/weapons/tnmmod/w_pistol.mdl"
+ITEM.class = "arccw_uspmatch2"
 ITEM.weaponCategory = "sidearm"
-ITEM.classes = {CLASS_EMP, CLASS_EOW}
 ITEM.flag = "V"
-ITEM.cpArmory = true;
 ITEM.width = 2
-ITEM.height = 1
-ITEM.iconCam = {
-	ang	= Angle(0.33879372477531, 270.15808105469, 0),
-	fov	= 5.0470897275697,
-	pos	= Vector(0, 200, -1)
-}
+ITEM.height = 2
+ITEM.cpArmory = true
 ITEM.pacData = {
 	[1] = {
 		["children"] = {
@@ -20,21 +14,72 @@ ITEM.pacData = {
 				["children"] = {
 				},
 				["self"] = {
-					-- x, y, z
-					["Angles"] = Angle(30,90,0),
-					["Position"] = Vector(-7, 0,1),
-					["UniqueID"] = "4249811628",
-					["Size"] = 1,
+					["Skin"] = 0,
+					["Invert"] = false,
+					["LightBlend"] = 1,
+					["CellShade"] = 0,
+					["OwnerName"] = "self",
+					["AimPartName"] = "",
+					["IgnoreZ"] = false,
+					["AimPartUID"] = "",
+					["Passes"] = 1,
+					["Name"] = "",
+					["NoTextureFiltering"] = false,
+					["DoubleFace"] = false,
+					["PositionOffset"] = Vector(0, 0, 0),
+					["IsDisturbing"] = false,
+					["Fullbright"] = false,
+					["EyeAngles"] = false,
+					["DrawOrder"] = 0,
+					["TintColor"] = Vector(0, 0, 0),
+					["UniqueID"] = "2050310462",
+					["Translucent"] = false,
+					["LodOverride"] = -1,
+					["BlurSpacing"] = 0,
+					["Alpha"] = 1,
+					["Material"] = "",
+					["UseWeaponColor"] = false,
+					["UsePlayerColor"] = false,
+					["UseLegacyScale"] = false,
 					["Bone"] = "pelvis",
-					["Model"] = "models/weapons/w_pistol.mdl",
+					["Color"] = Vector(255, 255, 255),
+					["Brightness"] = 1,
+					["BoneMerge"] = false,
+					["BlurLength"] = 0,
+					["Position"] = Vector(-4.34765625, -1.3203125, -1.6631927490234),
+					["AngleOffset"] = Angle(0, 0, 0),
+					["AlternativeScaling"] = false,
+					["Hide"] = false,
+					["OwnerEntity"] = false,
+					["Scale"] = Vector(1, 1, 1),
 					["ClassName"] = "model",
+					["EditorExpand"] = false,
+					["Size"] = 1,
+					["ModelFallback"] = "",
+					["Angles"] = Angle(-8.0638084411621, 85.052368164063, -85.48616027832),
+					["TextureFilter"] = 3,
+					["Model"] = "models/weapons/tnmmod/w_pistol.mdl",
+					["BlendMode"] = "",
 				},
 			},
 		},
 		["self"] = {
+			["DrawOrder"] = 0,
+			["UniqueID"] = "3332247455",
+			["AimPartUID"] = "",
+			["Hide"] = false,
+			["Duplicate"] = false,
 			["ClassName"] = "group",
-			["UniqueID"] = "907159818",
+			["OwnerName"] = "self",
+			["IsDisturbing"] = false,
+			["Name"] = "my outfit",
 			["EditorExpand"] = true,
 		},
 	},
+}
+
+ITEM.iconCam = {
+	ang	= Angle(-0.020070368424058, 270.40155029297, 0),
+	fov	= 7.2253324508038,
+	pos	= Vector(0, 200, -1)
 }
