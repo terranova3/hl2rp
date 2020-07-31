cc7967b30137e0444008e4f632c51d8332226d4e
Pointmanman <68431598+Pointmanman@users.noreply.github.com>
2020-07-31 16:56:41 +1000

    Update sh_ar2.lua
    UPDATE: New AR2 model + pacData, needs iconCam

--- ixhl2rp/schema/items/weapons/sh_ar2.lua
+++ ixhl2rp/schema/items/weapons/sh_ar2.lua
@@ -1,11 +1,83 @@
 ITEM.name = "Pulse-Rifle"
 ITEM.description = "A dark energy powered assault rifle."
-ITEM.model = "models/weapons/w_irifle.mdl"
-ITEM.class = "weapon_ar2"
+ITEM.model = "models/weapons/tnmmod/w_irifle.mdl"
+ITEM.class = "arccw_osipr"
 ITEM.weaponCategory = "primary"
 ITEM.classes = {CLASS_EOW}
 ITEM.width = 4
 ITEM.height = 2
+
+ITEM.pacData = {
+	[1] = {
+		["children"] = {
+			[1] = {
+				["children"] = {
+				},
+				["self"] = {
+					["Skin"] = 0,
+					["Invert"] = false,
+					["LightBlend"] = 1,
+					["CellShade"] = 0,
+					["OwnerName"] = "self",
+					["AimPartName"] = "",
+					["IgnoreZ"] = false,
+					["AimPartUID"] = "",
+					["Passes"] = 1,
+					["Name"] = "",
+					["NoTextureFiltering"] = false,
+					["DoubleFace"] = false,
+					["PositionOffset"] = Vector(0, 0, 0),
+					["IsDisturbing"] = false,
+					["Fullbright"] = false,
+					["EyeAngles"] = false,
+					["DrawOrder"] = 0,
+					["TintColor"] = Vector(0, 0, 0),
+					["UniqueID"] = "2690408469",
+					["Translucent"] = false,
+					["LodOverride"] = -1,
+					["BlurSpacing"] = 0,
+					["Alpha"] = 1,
+					["Material"] = "",
+					["UseWeaponColor"] = false,
+					["UsePlayerColor"] = false,
+					["UseLegacyScale"] = false,
+					["Bone"] = "spine",
+					["Color"] = Vector(255, 255, 255),
+					["Brightness"] = 1,
+					["BoneMerge"] = false,
+					["BlurLength"] = 0,
+					["Position"] = Vector(3.919921875, -6.4338989257813, 2.810791015625),
+					["AngleOffset"] = Angle(0, 0, 0),
+					["AlternativeScaling"] = false,
+					["Hide"] = false,
+					["OwnerEntity"] = false,
+					["Scale"] = Vector(1, 1, 1),
+					["ClassName"] = "model",
+					["EditorExpand"] = false,
+					["Size"] = 1,
+					["ModelFallback"] = "",
+					["Angles"] = Angle(26.343179702759, -0.25702786445618, -0.57948458194733),
+					["TextureFilter"] = 3,
+					["Model"] = "models/weapons/tnmmod/w_irifle.mdl",
+					["BlendMode"] = "",
+				},
+			},
+		},
+		["self"] = {
+			["DrawOrder"] = 0,
+			["UniqueID"] = "1320702186",
+			["AimPartUID"] = "",
+			["Hide"] = false,
+			["Duplicate"] = false,
+			["ClassName"] = "group",
+			["OwnerName"] = "self",
+			["IsDisturbing"] = false,
+			["Name"] = "my outfit",
+			["EditorExpand"] = true,
+		},
+	},
+}
+
 ITEM.iconCam = {
 	ang	= Angle(-0.70499622821808, 268.25439453125, 0),
 	fov	= 12.085652091515,
