f8d8aa201116ea8961b86f4c29940ab4a43237ad
Cooldeath91 <surfenbird@hotmail.com>
2020-07-31 21:34:13 +0800

    Update: Base Recipies

--- ixhl2rp/plugins/ixcraft/recipes/sh_recipe_watermelon.lua
+++ ixhl2rp/plugins/ixcraft/recipes/sh_recipe_watermelon.lua
@@ -4,10 +4,9 @@ RECIPE.description = "Craft a watermelon."
 RECIPE.model = "models/props_junk/watermelon01.mdl"
 RECIPE.category = "Watermelon"
 RECIPE.requirements = {
-	["water"] = 1
-}
+	["loyalgrademealkit"] = 1
 RECIPE.results = {
-	["melon"] = 1
+	["loyalgradesupppackage"] = 1
 }
 RECIPE.tools = {
 	"cid"
