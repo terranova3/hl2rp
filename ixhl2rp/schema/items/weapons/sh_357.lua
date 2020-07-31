9354558f5a68acc219c3e38404c7e21e7bec6d8b
Pointmanman <68431598+Pointmanman@users.noreply.github.com>
2020-07-31 16:56:05 +1000

    Update sh_357.lua
    UPDATE: New 357 model + pacData, needs iconCam

--- ixhl2rp/schema/items/weapons/sh_357.lua
+++ ixhl2rp/schema/items/weapons/sh_357.lua
@@ -1,12 +1,82 @@
-ITEM.name = "357"
-ITEM.description = "A sidearm utilising .357 Caliber ammunition."
-ITEM.model = "models/weapons/w_357.mdl"
-ITEM.class = "weapon_357"
+ITEM.name = "Colt Python Elite"
+ITEM.description = "A rare double-action revolver."
+ITEM.model = "models/weapons/tnmmod/w_357.mdl"
+ITEM.class = "arccw_357"
 ITEM.weaponCategory = "sidearm"
-ITEM.classes = {CLASS_EOW}
 ITEM.flag = "V"
 ITEM.width = 2
-ITEM.height = 1
+ITEM.height = 2
+ITEM.pacData = {
+	[1] = {
+		["children"] = {
+			[1] = {
+				["children"] = {
+				},
+				["self"] = {
+					["Skin"] = 0,
+					["Invert"] = false,
+					["LightBlend"] = 1,
+					["CellShade"] = 0,
+					["OwnerName"] = "self",
+					["AimPartName"] = "",
+					["IgnoreZ"] = false,
+					["AimPartUID"] = "",
+					["Passes"] = 1,
+					["Name"] = "",
+					["NoTextureFiltering"] = false,
+					["DoubleFace"] = false,
+					["PositionOffset"] = Vector(0, 0, 0),
+					["IsDisturbing"] = false,
+					["Fullbright"] = false,
+					["EyeAngles"] = false,
+					["DrawOrder"] = 0,
+					["TintColor"] = Vector(0, 0, 0),
+					["UniqueID"] = "2678518643",
+					["Translucent"] = false,
+					["LodOverride"] = -1,
+					["BlurSpacing"] = 0,
+					["Alpha"] = 1,
+					["Material"] = "",
+					["UseWeaponColor"] = false,
+					["UsePlayerColor"] = false,
+					["UseLegacyScale"] = false,
+					["Bone"] = "pelvis",
+					["Color"] = Vector(255, 255, 255),
+					["Brightness"] = 1,
+					["BoneMerge"] = false,
+					["BlurLength"] = 0,
+					["AlternativeScaling"] = false,
+					["Hide"] = false,
+					["OwnerEntity"] = false,
+					["Scale"] = Vector(1, 1, 1),
+					["Position"] = Vector(-7.5664367675781, -1.6357421875, -1.07568359375),
+					["AngleOffset"] = Angle(0, 0, 0),
+					["ClassName"] = "model",
+					["EditorExpand"] = false,
+					["Size"] = 1,
+					["ModelFallback"] = "",
+					["Angles"] = Angle(-0.00029329795506783, 89.458953857422, -0.0001656321255723),
+					["TextureFilter"] = 3,
+					["Model"] = "models/weapons/tnmmod/w_357.mdl",
+					["BlendMode"] = "",
+				},
+			},
+		},
+		["self"] = {
+			["DrawOrder"] = 0,
+			["UniqueID"] = "3398759426",
+			["AimPartUID"] = "",
+			["Hide"] = false,
+			["Duplicate"] = false,
+			["ClassName"] = "group",
+			["OwnerName"] = "self",
+			["IsDisturbing"] = false,
+			["Name"] = "my outfit",
+			["EditorExpand"] = true,
+		},
+	},
+}
+
 ITEM.iconCam = {
 	ang	= Angle(-17.581502914429, 250.7974395752, 0),
 	fov	= 5.412494001838,
