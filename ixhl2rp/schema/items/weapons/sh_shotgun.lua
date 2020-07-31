b4dbc96ccfe618edaa244fb7b4fdade3303f985f
Pointmanman <68431598+Pointmanman@users.noreply.github.com>
2020-07-31 16:57:56 +1000

    Update sh_shotgun.lua
    UPDATE: New shotgun model, pacData, needs iconCam

--- ixhl2rp/schema/items/weapons/sh_shotgun.lua
+++ ixhl2rp/schema/items/weapons/sh_shotgun.lua
@@ -1,11 +1,82 @@
 ITEM.name = "Shotgun"
 ITEM.description = "A powerful pump-action shotgun."
-ITEM.model = "models/weapons/w_shotgun.mdl"
-ITEM.class = "weapon_shotgun"
+ITEM.model = "models/weapons/tnmmod/w_shotgun.mdl"
+ITEM.class = "arccw_spas12terra"
 ITEM.weaponCategory = "primary"
 ITEM.classes = {CLASS_EOW}
-ITEM.width = 3
-ITEM.height = 1
+ITEM.width = 5
+ITEM.height = 2
+ITEM.pacData = {
+	[1] = {
+		["children"] = {
+			[1] = {
+				["children"] = {
+				},
+				["self"] = {
+					["Skin"] = 0,
+					["Invert"] = false,
+					["LightBlend"] = 1,
+					["CellShade"] = 0,
+					["OwnerName"] = "self",
+					["AimPartName"] = "",
+					["IgnoreZ"] = false,
+					["AimPartUID"] = "",
+					["Passes"] = 1,
+					["Name"] = "",
+					["NoTextureFiltering"] = false,
+					["DoubleFace"] = false,
+					["PositionOffset"] = Vector(0, 0, 0),
+					["IsDisturbing"] = false,
+					["Fullbright"] = false,
+					["EyeAngles"] = false,
+					["DrawOrder"] = 0,
+					["TintColor"] = Vector(0, 0, 0),
+					["UniqueID"] = "3196800276",
+					["Translucent"] = false,
+					["LodOverride"] = -1,
+					["BlurSpacing"] = 0,
+					["Alpha"] = 1,
+					["Material"] = "",
+					["UseWeaponColor"] = false,
+					["UsePlayerColor"] = false,
+					["UseLegacyScale"] = false,
+					["Bone"] = "spine",
+					["Color"] = Vector(255, 255, 255),
+					["Brightness"] = 1,
+					["BoneMerge"] = false,
+					["BlurLength"] = 0,
+					["Position"] = Vector(9.7060546875, -4.0811157226563, -0.068412780761719),
+					["AngleOffset"] = Angle(0, 0, 0),
+					["AlternativeScaling"] = false,
+					["Hide"] = false,
+					["OwnerEntity"] = false,
+					["Scale"] = Vector(1, 1, 1),
+					["ClassName"] = "model",
+					["EditorExpand"] = false,
+					["Size"] = 0.9,
+					["ModelFallback"] = "",
+					["Angles"] = Angle(31.623342514038, 8.4504747390747, -0.0010232086060569),
+					["TextureFilter"] = 3,
+					["Model"] = "models/weapons/tnmmod/w_shotgun.mdl",
+					["BlendMode"] = "",
+				},
+			},
+		},
+		["self"] = {
+			["DrawOrder"] = 0,
+			["UniqueID"] = "3903261695",
+			["AimPartUID"] = "",
+			["Hide"] = false,
+			["Duplicate"] = false,
+			["ClassName"] = "group",
+			["OwnerName"] = "self",
+			["IsDisturbing"] = false,
+			["Name"] = "my outfit",
+			["EditorExpand"] = true,
+		},
+	},
+}
+
 ITEM.iconCam = {
     pos = Vector(0, 200, 1),
     ang = Angle(0, 270, 0),
