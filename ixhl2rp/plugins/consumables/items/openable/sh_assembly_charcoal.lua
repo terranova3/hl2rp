2802aa64de36f1da5fc6cddfd0c62fb8e19caefc
Cooldeath91 <surfenbird@hotmail.com>
2020-08-01 13:36:01 +0800

    Fix: Workcycle 2

--- ixhl2rp/plugins/consumables/items/openable/sh_assembly_charcoal.lua
+++ ixhl2rp/plugins/consumables/items/openable/sh_assembly_charcoal.lua
@@ -1,5 +1,5 @@
-ITEM.name = "Designated Recyclables Box"
-ITEM.model = Model("models/props_junk/cardboard_box003a.mdll")
+ITEM.name = "Designated Charcoal Box"
+ITEM.model = Model("models/props_junk/cardboard_box003a.mdl")
 ITEM.description = "A large box labled with the recycle symbol, it contains junk items for refinement."
 ITEM.noBusiness = true
 ITEM.contains = {
