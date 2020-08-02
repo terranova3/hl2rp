043b87853cdaab77682f2181956cd2271591cd90
speckyaus <64631699+speckyaus@users.noreply.github.com>
2020-08-02 19:10:34 +1000

    UPDATE: added 2 firearms

---
+++ ixhl2rp/schema/items/weapons/sh_ak74.lua
@@ -0,0 +1,74 @@
+ITEM.name = "AK74"
+ITEM.description = "A classic 5.45mm calibre Russian assault rifle."
+ITEM.model = "models/weapons/arccw_insurgency/w_ak74.mdl"
+ITEM.class = "arccw_ins_ak74_tn"
+ITEM.weaponCategory = "primary"
+ITEM.classes = {CLASS_EOW}
+ITEM.width = 4
+ITEM.height = 2
+
+ITEM.pacData = {
+	[1] = {
+	["children"] = {
+		[1] = {
+			["children"] = {
+			},
+			["self"] = {
+				["Invert"] = false,
+				["EyeTargetName"] = "",
+				["NoLighting"] = false,
+				["OwnerName"] = "self",
+				["AimPartName"] = "",
+				["IgnoreZ"] = false,
+				["AimPartUID"] = "",
+				["Materials"] = "",
+				["Name"] = "",
+				["LevelOfDetail"] = 0,
+				["NoTextureFiltering"] = false,
+				["PositionOffset"] = Vector(0, 0, 0),
+				["NoCulling"] = false,
+				["Translucent"] = false,
+				["DrawOrder"] = 0,
+				["Alpha"] = 1,
+				["Material"] = "",
+				["Bone"] = "spine",
+				["UniqueID"] = "4131212387",
+				["BoneMerge"] = false,
+				["EyeTargetUID"] = "",
+				["Position"] = Vector(5.739501953125, -3.837890625, -0.1962890625),
+				["BlendMode"] = "",
+				["Angles"] = Angle(26.343000411987, -0.25699999928474, -0.5789999961853),
+				["Hide"] = false,
+				["EyeAngles"] = false,
+				["Scale"] = Vector(1, 1, 1),
+				["AngleOffset"] = Angle(0, 0, 0),
+				["EditorExpand"] = false,
+				["Size"] = 1,
+				["Color"] = Vector(1, 1, 1),
+				["ClassName"] = "model2",
+				["IsDisturbing"] = false,
+				["ModelModifiers"] = "",
+				["Model"] = "models/weapons/arccw_insurgency/w_ak74.mdl",
+			},
+		},
+	},
+	["self"] = {
+		["DrawOrder"] = 0,
+		["UniqueID"] = "1810603433",
+		["AimPartUID"] = "",
+		["Hide"] = false,
+		["Duplicate"] = false,
+		["ClassName"] = "group",
+		["OwnerName"] = "self",
+		["IsDisturbing"] = false,
+		["Name"] = "my outfit",
+		["EditorExpand"] = true,
+	},
+},
+
+
+ITEM.iconCam = {
+	ang	= Angle(-0.70499622821808, 268.25439453125, 0),
+	fov	= 12.085652091515,
+	pos	= Vector(0, 200, 0)
+}
