365e93abfd02b2231e210776264f101c15eba3d7
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-08-02 18:46:45 +1000

    UPDATE: Doubled paper character limit

--- ixhl2rp/plugins/writing/sh_plugin.lua
+++ ixhl2rp/plugins/writing/sh_plugin.lua
@@ -2,7 +2,7 @@
 PLUGIN.name = "Writing"
 PLUGIN.description = "Adds purchasable items which players can write/edit."
 PLUGIN.author = "`impulse"
-PLUGIN.maxLength = 512
+PLUGIN.maxLength = 1024
 
 ix.util.Include("sv_hooks.lua")
 ix.util.Include("cl_hooks.lua")
