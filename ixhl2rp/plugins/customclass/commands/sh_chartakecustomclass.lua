7db5becaa8a751e6d6838e40115c533a33f330ef
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-08-02 15:31:22 +1000

    ADDITION: CharTakeCustomClass

---
+++ ixhl2rp/plugins/customclass/commands/sh_chartakecustomclass.lua
@@ -0,0 +1,14 @@
+local PLUGIN = PLUGIN
+
+ix.command.Add("CharTakeCustomClass", {
+	adminOnly = true,
+	arguments = {
+		ix.type.character,
+		ix.type.string
+	},
+    OnRun = function(self, client, target, text)
+        target:SetCustomClass(nil);
+
+        client:Notify(string.format("You have taken the custom class from %s.", target:GetName(), text))
+	end
+})
