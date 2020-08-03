4331fea7416632f80b608b61bb81d753b50ba836
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-08-03 22:48:27 +1000

    UPDATE: Notepad remove dev debug

--- ixhl2rp/plugins/notepad/sv_plugin.lua
+++ ixhl2rp/plugins/notepad/sv_plugin.lua
@@ -10,17 +10,14 @@ netstream.Hook("EditNotepad", function(client, text, id)
 
 	-- Make sure the item exists and the item is a notepad if it does.
 	if(!item or item.uniqueID != "notepad") then
-		print("this is where")
 		return
 	end
 
-	print("got here")
 	-- Make sure the client requesting to edit the notepad is allowed to actually edit it.
 	if(item:GetData("character") and item:GetData("character") != client:GetCharacter():GetID()) then
 		return
 	end
 
-	print("win")
 	item:SetData("text", string.sub(text, 0, 1000));
 
 	if(!item:GetData("character")) then
