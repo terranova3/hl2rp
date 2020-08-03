14a08a3ef9fe3140392436e5d40be50ee6edf046
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-08-04 04:27:55 +1000

    UPDATE: Union card now redundant

--- ixhl2rp/plugins/unionlock/items/sh_union_card.lua
+++ ixhl2rp/plugins/unionlock/items/sh_union_card.lua
@@ -1,13 +1,13 @@
 ITEM.name = "Civil Access Card"
 ITEM.model = Model("models/dorado/tarjeta4.mdl")
-ITEM.description = "A machine manufactured card that gives the holder access to blue coded combine locks."
+ITEM.description = "An old machine manufactured card that used to give the holder access to blue coded combine locks. It had been disabled after the Civil Services card storage was raided."
 ITEM.factions = {FACTION_MPF, FACTION_OTA}
 ITEM.category = "Other"
 
 function ITEM:PopulateTooltip(tooltip)
 	local warning = tooltip:AddRow("warning")
 	warning:SetBackgroundColor(derma.GetColor("Error", tooltip))
-	warning:SetText("Contains an RFID chip. Used for gaining access to union locks.")
+	warning:SetText("Contains an RFID chip. This card has been disabled.")
 	warning:SetFont("DermaDefault")
 	warning:SetExpensiveShadow(0.5)
 	warning:SizeToContents()
