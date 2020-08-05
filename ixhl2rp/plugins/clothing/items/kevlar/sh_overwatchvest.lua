faffda3b1f3aa1728c3eaf4f41705dbef07b8c49
speckyaus <64631699+speckyaus@users.noreply.github.com>
2020-08-05 18:30:38 +1000

    FIX: removed item width and height from kevlar

--- ixhl2rp/plugins/clothing/items/kevlar/sh_overwatchvest.lua
+++ ixhl2rp/plugins/clothing/items/kevlar/sh_overwatchvest.lua
@@ -5,8 +5,6 @@
 
 ITEM.name = "Overwatch Vest";
 ITEM.price = 230;
-ITEM.width = 3;
-ITEM.height = 3;
 ITEM.model = "models/fty/items/otavest.mdl"
 ITEM.description = "Durable bullet resistant vest somehow salvaged from Overwatch."
 ITEM.flag = "G"
