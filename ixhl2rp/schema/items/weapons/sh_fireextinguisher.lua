2f3e387e65370eba922c594df6f6c91a9f75edbb
speckyaus <64631699+speckyaus@users.noreply.github.com>
2020-08-05 18:44:45 +1000

    UPDATE: added fire extinguisher iconcam

--- ixhl2rp/schema/items/weapons/sh_fireextinguisher.lua
+++ ixhl2rp/schema/items/weapons/sh_fireextinguisher.lua
@@ -6,3 +6,10 @@ ITEM.weaponCategory = "primary"
 ITEM.width = 2
 ITEM.height = 4
 ITEM.noBusiness = true
+
+
+ITEM.iconCam = {
+	pos = Vector(370.01300048828, 309.09985351563, 243.23098754883),
+	ang = Angle(25, 220, 0),
+	fov = 2.9411764705882,
+}
