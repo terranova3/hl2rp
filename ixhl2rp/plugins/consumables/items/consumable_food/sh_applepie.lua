a8ec8c969cd19e6b09aeb8788d5fbc2f8f79c475
Alex Bourikas <alexbksiad@gmail.com>
2020-08-06 22:25:43 +1000

    Major update - bakeries

---
+++ ixhl2rp/plugins/consumables/items/consumable_food/sh_applepie.lua
@@ -0,0 +1,15 @@
+--[[
+	© 2020 TERRANOVA do not share, re-distribute or modify
+	without permission of its author.
+--]]
+
+ITEM.name = "Apple Pie";
+ITEM.model = "models/foodnhouseholditems/pie.mdl";
+ITEM.width = 1;
+ITEM.height = 1;
+ITEM.description = "A deliciously sweet apple pie with crispy golden brown pie crust.";
+ITEM.permit = "consumables";
+ITEM.category = "Bread";
+ITEM.price = 25;
+ITEM.restoreHealth = 29;
+ITEM.flag = "b"
---
+++ ixhl2rp/plugins/consumables/items/consumable_food/sh_bread_rye.lua
@@ -0,0 +1,15 @@
+--[[
+	© 2020 TERRANOVA do not share, re-distribute or modify
+	without permission of its author.
+--]]
+
+ITEM.name = "Dark Rye Bread";
+ITEM.model = "models/foodnhouseholditems/bread-3.mdl";
+ITEM.width = 2;
+ITEM.height = 1;
+ITEM.description = "A loaf of delicious dark rye bread.";
+ITEM.permit = "consumables";
+ITEM.category = "Bread";
+ITEM.price = 18;
+ITEM.restoreHealth = 20;
+ITEM.flag = "b"
