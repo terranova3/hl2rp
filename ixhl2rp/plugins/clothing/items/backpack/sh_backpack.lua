99f8a3dd7289f78520999f852bea096eadee1a7a
fnn22 <64693109+fnn22@users.noreply.github.com>
2020-08-07 19:47:14 +0800

    UPDATE: backpack / satchel desc

--- ixhl2rp/plugins/clothing/items/backpack/sh_backpack.lua
+++ ixhl2rp/plugins/clothing/items/backpack/sh_backpack.lua
@@ -6,7 +6,7 @@
 ITEM.name = "Backpack";
 ITEM.model = "models/fty/items/flynnisstinkylol.mdl"
 ITEM.price = 25;
-ITEM.description = "Flynn give me a fucking description."
+ITEM.description = "A small sling bag, useful for carrying things."
 ITEM.outfitCategory = "bag";
 ITEM.backgroundColor = Color(19, 72, 96, 100)
 ITEM.flag = "a"
