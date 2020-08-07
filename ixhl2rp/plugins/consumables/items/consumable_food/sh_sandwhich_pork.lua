7c2744cd2f17fc53a476dd24422ab167fc9a0d07
Alex Bourikas <alexbksiad@gmail.com>
2020-08-07 23:05:20 +1000

    restoring balance to all things

---
+++ ixhl2rp/plugins/consumables/items/consumable_food/sh_sandwhich_pork.lua
@@ -0,0 +1,15 @@
+--[[
+	© 2020 TERRANOVA do not share, re-distribute or modify
+	without permission of its author.
+--]]
+
+ITEM.name = "Smoked Pulled Pork sandwich";
+ITEM.model = "models/foodnhouseholditems/sandwich.mdl";
+ITEM.width = 1;
+ITEM.height = 1;
+ITEM.description = "A mouth-watering sandwich with deliciously smoked pulled pork, fresh crispy tomato slices, and beautifully vibrant green lettuce leaves.";
+ITEM.permit = "consumables";
+ITEM.category = "Bread";
+ITEM.price = 35;
+ITEM.restoreHealth = 10;
+ITEM.flag = "b"
