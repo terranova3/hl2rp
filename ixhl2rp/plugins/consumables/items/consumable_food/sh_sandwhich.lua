a0d1d3a02bf8075d4dd8581a735fdd0c74b0f941
speckyaus <64631699+speckyaus@users.noreply.github.com>
2020-08-08 18:34:02 +1000

    UPDATE: adjusted sandwich name

--- ixhl2rp/plugins/consumables/items/consumable_food/sh_sandwhich.lua
+++ ixhl2rp/plugins/consumables/items/consumable_food/sh_sandwhich.lua
@@ -3,11 +3,11 @@
 	without permission of its author.
 --]]
 
-ITEM.name = "Chicken sandwich";
+ITEM.name = "Sandwich";
 ITEM.model = "models/foodnhouseholditems/sandwich.mdl";
 ITEM.width = 1;
 ITEM.height = 1;
-ITEM.description = "A mouth-watering sandwich with chicken meat slices, fresh crispy tomato slices, and beautifully vibrant green lettuce leaves.";
+ITEM.description = "A mouth-watering sandwich with various meat slices, fresh crispy tomato slices, and beautifully vibrant green lettuce leaves.";
 ITEM.permit = "consumables";
 ITEM.category = "Bread";
 ITEM.price = 35;
