d73a0a645985fdb3c3afcdb688be8b7a9c6afd45
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-08-09 03:45:29 +1000

    UPDATE: Item reorganisation into different plugins

---
+++ ixhl2rp/plugins/bakery/sh_plugin.lua
@@ -0,0 +1,8 @@
+--[[
+	© 2020 TERRANOVA do not share, re-distribute or modify
+	without permission of its author (zacharyenriquee@gmail.com).
+--]]
+
+PLUGIN.name = "Bakery";
+PLUGIN.description = "Adds numerous pastries and bread items.";
+PLUGIN.author = "Blackfire";
