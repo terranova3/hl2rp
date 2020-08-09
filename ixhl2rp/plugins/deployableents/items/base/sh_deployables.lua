d591470a9e57a7b454c3872d6ca541bce8adf20d
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-08-09 15:53:26 +1000

    FIX: Deployables showing in business menu

--- ixhl2rp/plugins/deployableents/items/base/sh_deployables.lua
+++ ixhl2rp/plugins/deployableents/items/base/sh_deployables.lua
@@ -5,21 +5,22 @@ ITEM.entityName = "npc_turret_floor"
 ITEM.description = "Deploy It"
 ITEM.width = 2
 ITEM.height = 2
-
+ITEM.noBusiness = true
+ITEM.price = 20
 ITEM.functions.Deploy = {
 	OnRun = function(item)
         if item.entityName then
-            local pos = item.entity:GetPos()
-            pos:Add(Vector(0,0,50))//prevention for stuck ents inside  world
+            local pos = item.entity:GetPos():Add(Vector(0, 0, 50))
+
             local spawned = ents.Create(item.entityName)
             spawned:SetAngles(item.player:GetAngles())
             spawned:SetPos(pos)
             spawned:Spawn()
+
             return true
         end
-		
 	end,
 	OnCanRun = function(item)
-                return IsValid(item.entity)
+        return IsValid(item.entity)
 	end
 }
