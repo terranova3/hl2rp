a13b2150408637e27b68b0be6a6ff8311383bc70
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-08-12 03:59:22 +1000

    ADDITION: /ClearDecals

---
+++ ixhl2rp/plugins/aframeworkedits/commands/sh_cleardecals.lua
@@ -0,0 +1,17 @@
+--[[
+	© 2020 TERRANOVA do not share, re-distribute or modify
+	without permission of its author (zacharyenriquee@gmail.com).
+--]]
+
+local PLUGIN = PLUGIN
+
+ix.command.Add("ClearDecals", {
+    description = "Clears all decals for all players.",
+    adminOnly = true,
+    OnRun = function(self, client)
+		for k, v in pairs(player.GetAll()) do
+			v:ConCommand("r_cleardecals");
+			client:Notify("You have cleared all decals.")
+		end;
+	end;
+})
