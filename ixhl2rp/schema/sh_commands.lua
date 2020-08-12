c36cad72039c28489a30ef6ee1da22013f473b38
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-08-12 17:40:55 +1000

    ADDITION: /R command for radio

--- ixhl2rp/schema/sh_commands.lua
+++ ixhl2rp/schema/sh_commands.lua
@@ -48,6 +48,39 @@ end
 
 do
 	local COMMAND = {}
+	COMMAND.arguments = ix.type.text
+
+	function COMMAND:OnRun(client, message)
+		local character = client:GetCharacter()
+		local radios = character:GetInventory():GetItemsByUniqueID("handheld_radio", true)
+		local item
+
+		for k, v in ipairs(radios) do
+			if (v:GetData("enabled", false)) then
+				item = v
+				break
+			end
+		end
+
+		if (item) then
+			if (!client:IsRestricted()) then
+				ix.chat.Send(client, "radio", message)
+				ix.chat.Send(client, "radio_eavesdrop", message)
+			else
+				return "@notNow"
+			end
+		elseif (#radios > 0) then
+			return "@radioNotOn"
+		else
+			return "@radioRequired"
+		end
+	end
+
+	ix.command.Add("R", COMMAND)
+end
+
+do
+	local COMMAND = {}
 	COMMAND.arguments = ix.type.number
 
 	function COMMAND:OnRun(client, frequency)
