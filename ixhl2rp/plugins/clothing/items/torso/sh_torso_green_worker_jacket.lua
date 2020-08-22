0f4d7d3bc8823ccc85497030cdbce53e1abcd201
speckyaus <64631699+speckyaus@users.noreply.github.com>
2020-08-22 21:45:05 +1000

    UPDATE: moved clothing category & flag into mcs

--- ixhl2rp/plugins/clothing/items/torso/sh_torso_green_worker_jacket.lua
+++ ixhl2rp/plugins/clothing/items/torso/sh_torso_green_worker_jacket.lua
@@ -8,7 +8,8 @@ ITEM.name = "Worker's Green Jacket";
 ITEM.model = "models/fty/items/betaworkshirt.mdl"
 ITEM.price = 12;
 ITEM.description = "A worker's green jacket made from a coarse material similar to denim, with a light nylon lining. There's a tag of white fabric attached to the inside of the collar near the neck, reading 'CWU-Approved'.";
-ITEM.flag = "a"
+ITEM.flag = "A"
+ITEM.category = "Clothing - MCS";
 ITEM.bodyGroups = {
 	["torso"] = 18
 }
