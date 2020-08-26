beb35ba457fe44a9faf37b44ab784d8d340bf276
speckyaus <64631699+speckyaus@users.noreply.github.com>
2020-08-26 13:34:51 +1000

    FIX: corrected rebel clothes category, flags & made visible

--- ixhl2rp/plugins/clothing/items/legs/sh_rebel_jeans_2.lua
+++ ixhl2rp/plugins/clothing/items/legs/sh_rebel_jeans_2.lua
@@ -6,9 +6,10 @@
 ITEM.base = "base_legs";
 ITEM.name = "Rebel Legs - 2";
 ITEM.model = "models/fty/items/bluerebelpants.mdl"
-ITEM.price = 14
+ITEM.price = 35
+ITEM.flag = "G"
+ITEM.category = "Clothing - Contraband";
 ITEM.description = "Fresh and vibrant in appearance, re-purposed levis made to aid the active rebellion against the opposition, fitted with multi-purpose pouches able to house ammunition and small arms while tucked into the ankles of some combat boots."
-ITEM.noBusiness = true
 ITEM.bodyGroups = {
 	["legs"] = 6
 }
