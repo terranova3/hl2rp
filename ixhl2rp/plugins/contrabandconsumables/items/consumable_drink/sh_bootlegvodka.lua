647fa18b0d75f37423a1be383d2697e1a6874e64
speckyaus <64631699+speckyaus@users.noreply.github.com>
2020-08-28 19:13:17 +1000

    UPDATE: changed item height & width for bootleg stuff

--- ixhl2rp/plugins/contrabandconsumables/items/consumable_drink/sh_bootlegvodka.lua
+++ ixhl2rp/plugins/contrabandconsumables/items/consumable_drink/sh_bootlegvodka.lua
@@ -6,7 +6,7 @@
 ITEM.name = "Bootleg Vodka";
 ITEM.model = "models/teebeutel/metro/objects/bottle03.mdl";
 ITEM.width = 1;
-ITEM.height = 1;
+ITEM.height = 2;
 ITEM.description = "A clear alcoholic drink that's supposed to be flavourless and scentless. This is neither.";
 ITEM.permit = "consumables";
 ITEM.price = 5;
