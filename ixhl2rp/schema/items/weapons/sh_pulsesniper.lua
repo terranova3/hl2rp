c6ee24874a64941144f743a24e6f43c507cb7e5c
speckyaus <64631699+speckyaus@users.noreply.github.com>
2020-09-02 22:22:53 +1000

    UPDATE: pulse sniper added

---
+++ ixhl2rp/schema/items/weapons/sh_pulsesniper.lua
@@ -0,0 +1,75 @@
+ITEM.name = "OSIPMR"
+ITEM.description = "A dark energy powered sniper rifle."
+ITEM.model = "models/weapons/w_assaultsniper.mdl"
+ITEM.class = "arccw_pulsesniper_tn"
+ITEM.weaponCategory = "primary"
+ITEM.classes = {CLASS_EOW}
+ITEM.width = 4
+ITEM.height = 2
+ITEM.noBusiness = true
+
+ITEM.pacData = {
+	[1] = {
+		["children"] = {
+			[1] = {
+				["children"] = {
+				},
+				["self"] = {
+					["Invert"] = false,
+					["EyeTargetName"] = "",
+					["NoLighting"] = false,
+					["OwnerName"] = "self",
+					["AimPartName"] = "",
+					["IgnoreZ"] = false,
+					["AimPartUID"] = "",
+					["Materials"] = "",
+					["Name"] = "",
+					["LevelOfDetail"] = 0,
+					["NoTextureFiltering"] = false,
+					["PositionOffset"] = Vector(0, 0, 0),
+					["NoCulling"] = false,
+					["Translucent"] = false,
+					["DrawOrder"] = 0,
+					["Alpha"] = 1,
+					["Material"] = "",
+					["Bone"] = "spine",
+					["UniqueID"] = "2961833181",
+					["BoneMerge"] = false,
+					["EyeTargetUID"] = "",
+					["Position"] = Vector(17.3359375, -1.4512634277344, -7.8893432617188),
+					["BlendMode"] = "",
+					["Angles"] = Angle(-22.310424804688, -160.15397644043, -16.494787216187),
+					["Hide"] = false,
+					["EyeAngles"] = false,
+					["Scale"] = Vector(1, 1, 1),
+					["AngleOffset"] = Angle(0, 0, 0),
+					["EditorExpand"] = false,
+					["Size"] = 1,
+					["Color"] = Vector(1, 1, 1),
+					["ClassName"] = "model2",
+					["IsDisturbing"] = false,
+					["ModelModifiers"] = "",
+					["Model"] = "models/weapons/w_assaultsniper.mdl",
+				},
+			},
+		},
+		["self"] = {
+			["DrawOrder"] = 0,
+			["UniqueID"] = "2864977925",
+			["AimPartUID"] = "",
+			["Hide"] = false,
+			["Duplicate"] = false,
+			["ClassName"] = "group",
+			["OwnerName"] = "self",
+			["IsDisturbing"] = false,
+			["Name"] = "my outfit",
+			["EditorExpand"] = true,
+		},
+	},
+}
+
+ITEM.iconCam = {
+	pos = Vector(0, 200, 0),
+	ang = Angle(0, 270, 8),
+	fov = 14.705882352941,
+}
