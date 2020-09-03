1070a4fe61fa8d8b2528fa7748f7e55d8de0cc8f
speckyaus <64631699+speckyaus@users.noreply.github.com>
2020-09-03 18:14:19 +1000

    UPDATE: gas grenade weapon class edit

--- ixhl2rp/schema/items/weapons/sh_gasgrenade.lua
+++ ixhl2rp/schema/items/weapons/sh_gasgrenade.lua
@@ -1,7 +1,7 @@
 ITEM.name = "Gas Grenade"
 ITEM.description = "Gas grenade used for crowd dispersion."
 ITEM.model = "models/weapons/w_eq_smokegrenade.mdl"
-ITEM.class = "arccw_nade_gas"
+ITEM.class = "arccw_nade_gas_tn"
 ITEM.weaponCategory = "grenade"
 ITEM.width = 1
 ITEM.height = 1
