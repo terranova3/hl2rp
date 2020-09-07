0ee52db7e59aff41749b576ca74de5e2277e875c
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-09-07 22:12:01 +1000

    ADDITION: Cigarette pacData override

--- ixhl2rp/plugins/cigarettes/items/cigarettes/sh_unioncigarette.lua
+++ ixhl2rp/plugins/cigarettes/items/cigarettes/sh_unioncigarette.lua
@@ -3,6 +3,12 @@
 	without permission of its author.
 --]]
 
+local unlit = table.Copy(PLUGIN.pacData.unlit)
+unlit[1]["children"][1]["self"]["model"] = "models/phycitnew.mdl"
+
+local lit = table.Copy(PLUGIN.pacData.lit)
+lit[1]["children"][1]["self"]["model"] = "models/phycitnew.mdl"
+
 ITEM.name = "Approved Cigarette"
 ITEM.model = Model("models/phycitnew.mdl")
 ITEM.width = 1
@@ -12,3 +18,8 @@ ITEM.category = "Other"
 ITEM.flag = "g"
 ITEM.price = 3;
 ITEM.time = 90
+ITEM.pacData = unlit
+ITEM.pacDataAlternate = lit
+
+unlit = nil
+lit = nil
