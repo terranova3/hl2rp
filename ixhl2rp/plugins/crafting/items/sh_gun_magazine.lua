5d0443fbf32ab9fafc6abdb673c4c5c3465e7632
zacharyenriquee <48750526+zacharyenriquee@users.noreply.github.com>
2020-09-07 23:15:47 +1000

    UPDATE: Magazine model

--- ixhl2rp/plugins/crafting/items/sh_gun_magazine.lua
+++ ixhl2rp/plugins/crafting/items/sh_gun_magazine.lua
@@ -1,6 +1,6 @@
 
 ITEM.name = "Magazine"
-ITEM.model = Model("models/weapons/tfa_mmod/w_smg1_magazine.mdl")
+ITEM.model = Model("models/weapons/tnmmod/w_pistol_magazine.mdl")
 ITEM.description = "A magazine clip used to store ammunition, this one is loaded."
 ITEM.width = 1
 ITEM.height = 1
