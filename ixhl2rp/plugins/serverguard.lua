81b43c0c79412da8029a62ccad497baeadcf93c2
zacharyenriquee <zacharyenriquee@gmail.com>
2023-02-14 02:55:14 +1100

    FIX: Serverguard error on launch

--- ixhl2rp/plugins/serverguard.lua
+++ ixhl2rp/plugins/serverguard.lua
@@ -3,25 +3,27 @@ PLUGIN.author = "alexgrist"
 PLUGIN.desc = "Adds CAMI permission compatibility to ServerGuard."
 
 local function OnPrivilegeRegistered(privilege)
-    local permission = privilege.Name
-    serverguard.permission:Add(permission)
+	if(SERVERGUARD) then
+		local permission = privilege.Name
+		serverguard.permission:Add(permission)
 
-    if (SERVER) then
-        local defaultRank = privilege.MinAccess
-        defaultRank = defaultRank:sub(1, 1):upper() .. defaultRank:sub(2)
+		if (SERVER) then
+			local defaultRank = privilege.MinAccess
+			defaultRank = defaultRank:sub(1, 1):upper() .. defaultRank:sub(2)
 
-        for rank, _ in pairs(serverguard.ranks:GetStored()) do
-            -- we're setting defaults here, so only give permission if it hasn't been manually set already (i.e nil)
-            if (serverguard.ranks:HasPermission(rank, permission) == nil and
-                (defaultRank == "User" or serverguard.ranks:HasPermission(rank, defaultRank))) then
-                serverguard.ranks:GivePermission(rank, permission)
-            end
-        end
-    end
+			for rank, _ in pairs(serverguard.ranks:GetStored()) do
+				-- we're setting defaults here, so only give permission if it hasn't been manually set already (i.e nil)
+				if (serverguard.ranks:HasPermission(rank, permission) == nil and
+					(defaultRank == "User" or serverguard.ranks:HasPermission(rank, defaultRank))) then
+					serverguard.ranks:GivePermission(rank, permission)
+				end
+			end
+		end
+	end
 end
 
 local function RegisterPrivileges()
-    if (CAMI) then
+    if (CAMI and SERVERGUARD) then
         -- register privileges that may have been added before we've loaded
         for _, v in pairs(CAMI.GetPrivileges()) do
             OnPrivilegeRegistered(v)
